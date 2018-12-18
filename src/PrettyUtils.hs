38634c07a5d2447c2d9924c2a7db4acdb328aa76
Siddharth Bhat <siddu.druid@gmail.com>
2018-12-18 17:01:12 +0530

    cannibalize infrastructure from tiny-optimising-compiler for a builder

---
+++ src/PrettyUtils.hs
@@ -0,0 +1,16 @@
+module PrettyUtils where
+import Data.Text.Prettyprint.Doc.Render.Text
+import qualified Data.Text.Lazy as L
+import Data.Text.Prettyprint.Doc
+
+docToText :: Doc ann -> L.Text
+docToText doc = renderLazy (layoutPretty defaultLayoutOptions doc)
+
+docToString :: Doc ann -> String
+docToString = L.unpack . docToText
+
+prettyableToText :: Pretty a => a -> L.Text
+prettyableToText a = docToText (pretty a)
+
+prettyableToString :: Pretty a => a -> String
+prettyableToString  a = docToString (pretty a)
