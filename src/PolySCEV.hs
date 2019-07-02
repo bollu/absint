cfe4fda428b7750e02ceda1f41707b672ad39b4a
Siddharth Bhat <siddharth.bath@tweag.io>
2019-07-02 20:48:45 +0200

    Add current version. Dies at <<loop>>

--- src/PolySCEV.hs
+++ src/PolySCEV.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 module PolySCEV(V, AI, mkAI) where
 import ISL.Native.C2Hs
 import ISL.Native.Types (DimType(..),
@@ -6,25 +7,42 @@ import ISL.Native.Types (DimType(..),
 import qualified ISL.Native.Types as ISLTy (Id)
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
+import Data.Maybe
 import IR
 import Lattice
 import Absdomain
 import Foreign.Ptr
 import Interpreter
+import System.IO.Unsafe
+
+gctx :: Ptr Ctx
+gctx = unsafePerformIO $ do
+  c <- ctxAlloc
+  islAbortOnError gctx
+  return c
+  
+
+-- | Short alias
+uio :: IO a -> a
+uio = unsafePerformIO
 
 
+-- | Pwaff
+newtype P = P (Ptr Pwaff) deriving(Show)
+
+
+instance Eq P where
+  P p == P p' = uio $ pwaffIsEqual p p'
+
+-- | Sets
+newtype S = S (Ptr Set) deriving(Show)
+
+instance Eq S where
+  S s == S s' =  fromJust $ uio $ setIsEqual s s'
 
 -- | abstract value
-data V = V { vsym :: Pwaff -- ^ pwsymbolic value, with _no acceleration_ at all
-             , vacc :: Pwaff -- ^ accelerated value.
-             , vaccloops :: S.Set Id -- ^ accelerated against which loops
-           }
-
-instance Eq V where
-    (==) = undefined
-
-instance Show V where
-    show = undefined
+data V = V { vsym :: P -- ^ pwsymbolic value, with _no acceleration_ at all
+           } deriving(Eq, Show)
 
 instance Lattice V where
     lbot = undefined
@@ -38,41 +56,64 @@ instance Lattice V where
 data G = G { gparams :: S.Set Id -- ^ formal parameters
            , gvivs :: S.Set Id -- ^ IDs of only loops
            , gvars :: S.Set Id -- ^ regular variables
+           , gall :: S.Set Id
            , glocs :: S.Set Loc -- ^ locations in the program
-           , islctx :: Ptr Ctx -- ^ ISL context
+           , gislids :: M.Map Id (Ptr ISLTy.Id) -- ^ ISL ids
+           , gls :: Ptr LocalSpace -- ^ localspace that is of the right shape
            }
 
 -- | New local space
-ls :: G -> LocalSpace
-ls = undefined
+-- ls :: G -> LocalSpace
+-- ls = unsafePerformIO $ do
+--   s <- spaceSetAlloc ctx 0 (length gall)
+--   reurn undefined
+  
 
 -- | Pwaff that is purely pwsymbolic
-pwsymbolic :: G -> Id -> V
-pwsymbolic g@G{..} id = undefined
+pwsymbolic :: G -> Id -> Ptr Pwaff
+pwsymbolic g@G{..} id = uio $ do
+  Just ix <- findDimById gls IslDimSet (gislids M.! id)
+  affVarOnDomain gls IslDimSet ix >>= pwaffFromAff
+    
+-- | Create a pwaff that takes on the value of the variable
+-- pwVar :: Ptr Ctx -> OM.OrderedMap Id (Ptr ISLTy.Id) -> Id -> IO (Ptr Pwaff)
+-- pwVar ctx id2isl id = do
+--   ls <- absSetSpace ctx id2isl >>= localSpaceFromSpace
+--   Just ix <- findDimById ls IslDimSet (id2isl OM.! id)
+--   affVarOnDomain ls IslDimSet ix >>= pwaffFromAff
+
 
 -- | Pwaff that is undefined everywhere
-pwundef :: G -> Id -> V
-pwundef g@G{..} id = undefined
+pwundef :: G -> Ptr Pwaff
+pwundef g@G{..} = undefined
 
 -- | Pwaff that is constant everywhere
-constant :: G -> Int -> V
-constant g@G{..} id = undefined
+constant :: G -> Int -> Ptr Pwaff
+constant g@G{..} i = uio $ do
+  pwaffInt gctx gls  i
+ 
 
+-- | Create ISL ids
+mkIslIds :: [Id] -> IO [(Id, Ptr ISLTy.Id)]
+mkIslIds ids =
+  traverse (\id -> (id, ) <$> idAlloc gctx (show id)) ids
+  
 -- | Create a G instance for a given program.
 mkG :: Program -> IO G
 mkG p = do
-    islctx <- ctxAlloc
-    islAbortOnError islctx
     let gvars = progvarids p
     let gparams = progparams p
     let gvivs = progvivs p
     let glocs = S.fromList $ progbbs p >>= bbGetLocs
+    let gall = gvivs `S.union` gparams `S.union` gvars
+    gislids <- M.fromList <$> mkIslIds (S.toList gall)
+    gls <- localSpaceIds gctx gislids
     return $ G{..}
 
 -- | Abstract interpret an expression
 aie :: G -> Expr -> AbsDom V -> V
 aie G{..} (EInt i) = undefined
-aie G{..} (EId id) =undefined
+aie G{..} (EId id) = undefined
 aie G{..} (EBinop  op e1 e2) = undefined
 
 
@@ -89,8 +130,8 @@ aistart g@G{..} =
   let symvars = gparams
       botvars = gvars
       d = lmfromlist $
-            [ (id, pwundef g id) | id <- S.toList gvars] <>
-            [ (id, pwsymbolic g id) | id <- S.toList symvars] :: AbsDom V
+            [ (id, V $ P $ pwundef g) | id <- S.toList gvars] <>
+            [ (id, V $ P $ pwsymbolic g id) | id <- S.toList symvars] :: AbsDom V
 
   -- | Each abstract state has
   -- the same abstract domain: everything is pwsymbolic
